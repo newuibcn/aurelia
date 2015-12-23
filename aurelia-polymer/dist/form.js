df6e35cd15f9ee3c1a2dca14124f56b404da9516
Alex Vizcaino <avizcainoluna@gmail.com>
2015-12-23 17:06:25 +0100

    add form

---
+++ aurelia-polymer/dist/form.js
@@ -0,0 +1,19 @@
+System.register([], function(exports_1) {
+    var Form;
+    return {
+        setters:[],
+        execute: function() {
+            Form = (function () {
+                function Form() {
+                }
+                Form.prototype.send = function () {
+                    $('#sent-form')[0].open();
+                };
+                return Form;
+            })();
+            exports_1("Form", Form);
+        }
+    }
+});
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZvcm0udHMiXSwibmFtZXMiOlsiRm9ybSIsIkZvcm0uY29uc3RydWN0b3IiLCJGb3JtLnNlbmQiXSwibWFwcGluZ3MiOiI7Ozs7O1lBR0E7Z0JBQUFBO2dCQUlBQyxDQUFDQTtnQkFIR0QsbUJBQUlBLEdBQUpBO29CQUNJRSxDQUFDQSxDQUFDQSxZQUFZQSxDQUFDQSxDQUFDQSxDQUFDQSxDQUFDQSxDQUFDQSxJQUFJQSxFQUFFQSxDQUFDQTtnQkFDOUJBLENBQUNBO2dCQUNMRixXQUFDQTtZQUFEQSxDQUpBLEFBSUNBLElBQUE7WUFKRCx1QkFJQyxDQUFBIiwiZmlsZSI6ImZvcm0uanMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcclxuICogQ3JlYXRlZCBieSBBbGV4IG9uIDIzLzEyLzIwMTUuXHJcbiAqL1xyXG5leHBvcnQgY2xhc3MgRm9ybXtcclxuICAgIHNlbmQoKXtcclxuICAgICAgICAkKCcjc2VudC1mb3JtJylbMF0ub3BlbigpO1xyXG4gICAgfVxyXG59Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
